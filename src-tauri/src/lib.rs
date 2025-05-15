6011b6e39e84fa3adc7c1b1f25f489a41c3b5022
dan-lund <danjadon4@gmail.com>
2025-05-15 20:58:09 +0100

    Clean up

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -67,7 +67,6 @@ fn load_model_from_bytes(image_bytes: Vec<u8>) -> Result<String> {
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
-        .plugin(tauri_plugin_opener::init())
         .invoke_handler(tauri::generate_handler![
             try_load_model 
         ])
